c62cac2a67f4bb5cff0e0d0cdea193fdfec62a31
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-10 17:49:07 +0200

    getWinners

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,6 +9,7 @@ const funcs = require('../index.js')
 
 describe('getFinals', () => {
   const result = funcs.getFinals(data)
+  console.log(result)
 
   it('returns the right number of matches', () => {
     expect(result).toHaveLength(20)
@@ -24,7 +25,7 @@ describe('getFinals', () => {
 
 describe('getYears', () => {
   const result = funcs.getYears(data, funcs.getFinals)
-  console.log(result)
+
   it('returns the right number of years', () => {
     expect(result).toHaveLength(20)
   })
@@ -35,6 +36,29 @@ describe('getYears', () => {
       1974, 1978, 1982, 1986,
       1990, 1994, 1998, 2002,
       2006, 2010, 2014, 2014,
+    ]))
+  })
+})
+
+describe('getWinners', () => {
+  const result = funcs.getWinners(data, funcs.getFinals)
+  console.log(result)
+
+  it('returns the right number of countries', () => {
+    expect(result).toHaveLength(20)
+  })
+  it('returns the right countries', () => {
+    expect(result).toEqual(expect.arrayContaining([
+      'Uruguay', 'Italy',
+      'Italy', 'Germany FR',
+      'Brazil', 'Brazil',
+      'England', 'Brazil',
+      'Germany FR', 'Argentina',
+      'Italy', 'Argentina',
+      'Germany FR', 'Brazil',
+      'France', 'Brazil',
+      'Italy', 'Spain',
+      'Germany', 'Germany'
     ]))
   })
 })
