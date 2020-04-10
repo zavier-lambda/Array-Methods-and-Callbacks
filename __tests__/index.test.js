c0f9bf06e79e406c19dc6ac312aed1af5afe6e50
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-10 16:32:03 +0200

    getYears

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,6 +9,7 @@ const funcs = require('../index.js')
 
 describe('getFinals', () => {
   const result = funcs.getFinals(data)
+
   it('returns the right number of matches', () => {
     expect(result).toHaveLength(20)
   })
@@ -22,5 +23,18 @@ describe('getFinals', () => {
 })
 
 describe('getYears', () => {
-  
+  const result = funcs.getYears(data, funcs.getFinals)
+  console.log(result)
+  it('returns the right number of years', () => {
+    expect(result).toHaveLength(20)
+  })
+  it('returns the right years', () => {
+    expect(result).toEqual(expect.arrayContaining([
+      1930, 1934, 1938, 1954,
+      1958, 1962, 1966, 1970,
+      1974, 1978, 1982, 1986,
+      1990, 1994, 1998, 2002,
+      2006, 2010, 2014, 2014,
+    ]))
+  })
 })
